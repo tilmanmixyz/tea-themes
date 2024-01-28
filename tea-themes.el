2fa0a06c9629879c23db40a9f6e3f713beaf1a00
Tilman Andre Mix <tilman@mixhamburg.de>
2024-01-28 17:34:45 +0000

    actually provide the themes

--- tea-themes.el
+++ tea-themes.el
@@ -70,5 +70,13 @@
    theme-name
    '(default ((,class (:background ,tea-bg :foreground ,tea-fg))))))
 
+;;; Provide file
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
+
 (provide 'tea-themes)
 ;;; tea-themes.el ends here
