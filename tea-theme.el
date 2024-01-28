7e27557512ca37617b30548ef2ce28844ff017a0
Tilman Andre Mix <tilmanmixyz@proton.me>
2024-01-28 16:43:43 +0000

    add simple project setup

---
+++ tea-theme.el
@@ -0,0 +1,36 @@
+;;; tea-theme.el --- A custom theme  -*- lexical-binding: t; -*-
+;; Copyright (C) 2024 Tilman Andre Mix
+;; -------------------------------------------------------------------
+;; Authors: Colin McLear
+;; -------------------------------------------------------------------
+;; URL: https://github.com/mclear-tools/lambda-themes
+;; -------------------------------------------------------------------
+;; Created: 2021-03-16
+;; Version: 0.1
+;; Package-Requires: ((emacs "25.1"))
+;; -------------------------------------------------------------------
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the BSD Zero Clause License
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://opensource.org/license/0bsd/>
+;; -------------------------------------------------------------------
+;;; Commentary:
+;; Tea-Theme provides a simple theme for GNU EMacs
+;; -------------------------------------------------------------------
+
+;;; Code:
+
+;;;; Requirements
+(eval-when-compile
+  (require 'cl-macs))
+
+(provide 'tea-theme)
+;;; tea-theme.el ends here
